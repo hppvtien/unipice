ee9b75fb87abc51eb1e922925cade65fb9e330bd
ducanh30091995hpv <ducanh30091995hpv@gmail.com>
2021-07-22 17:43:57 +0700

    user_product_list_filter

--- Modules/User/Resources/views/pages/dashboard/product_list.blade.php
+++ Modules/User/Resources/views/pages/dashboard/product_list.blade.php
@@ -22,14 +22,14 @@
                 </div>
                 <div class="content block-collapsible-nav-content" id="block-collapsible-nav">
                     <ul class="nav items">
-                        <li class="nav item"><a href="https://shop.coopmarket.com/sales/order/history/">My Orders</a></li>
-                        <li class="nav item"><a href="https://shop.coopmarket.com/wishlist/">My Wish List</a></li>
-                        <li class="nav item"><a href="https://shop.coopmarket.com/customer/address/">Address Book</a></li>
+                        <li class="nav item liststyle_none"><a href="https://shop.coopmarket.com/sales/order/history/">My Orders</a></li>
+                        <li class="nav item liststyle_none"><a href="https://shop.coopmarket.com/wishlist/">My Wish List</a></li>
+                        <li class="nav item liststyle_none"><a href="https://shop.coopmarket.com/customer/address/">Address Book</a></li>
 
-                        <li class="nav item"><a href="https://shop.coopmarket.com/customer/account/edit/">Account Information</a></li>
-                        <li class="nav item"><a href="https://shop.coopmarket.com/newsletter/manage/">Newsletter Subscriptions</a></li>
-                        <li class="nav item"><a href="https://shop.coopmarket.com/favorites/">My Favorites</a></li>
-                        <li class="nav item"><a href="{{ route('get_user.productlist') }}">List Sản Phẩm</a></li>
+                        <li class="nav item liststyle_none"><a href="https://shop.coopmarket.com/customer/account/edit/">Account Information</a></li>
+                        <li class="nav item liststyle_none"><a href="https://shop.coopmarket.com/newsletter/manage/">Newsletter Subscriptions</a></li>
+                        <li class="nav item liststyle_none"><a href="https://shop.coopmarket.com/favorites/">My Favorites</a></li>
+                        <li class="nav item liststyle_none"><a href="{{ route('get_user.productlist') }}">List Sản Phẩm</a></li>
 
                     </ul>
                 </div>
@@ -41,7 +41,7 @@
 
                     <div class="c-filter-bar__sorting">
                         <div class="m-sort-by">
-                            <select class="m-sort-by__select frontier-custom-sort" id="sort_by" name="sort_by" aria-label="Sort By">
+                            <select onchange="get_data1();" class="m-sort-by__select frontier-custom-sort" id="sort_by" name="sort_by" aria-label="Sort By">
                                 <option value="0" selected="">Chọn Danh Mục Sản Phẩm</option>
                                 @foreach ($category_menu as $l)
                                     <option value="{{ $l->id }}">{{ $l->name }}</option>
@@ -50,10 +50,10 @@
                             <span class="m-sort-by__arrow"></span>
                         </div>
                         <div class="m-sort-by">
-                            <select class="m-sort-by__select frontier-custom-sort" id="order_by" name="order_by" aria-label="Order By">
+                            <select onchange="get_data1();" class="m-sort-by__select frontier-custom-sort" id="order_by" name="order_by" aria-label="Order By">
                                 <option value="0" selected="">Chọn Sắp Xếp</option>
-                                <option value="1" selected="">Tăng</option>
-                                <option value="2">Giảm</option>
+                                <option value="desc" selected="">Tăng</option>
+                                <option value="asc">Giảm</option>
                             </select>
                             <span class="m-sort-by__arrow"></span>
                         </div>
@@ -61,11 +61,12 @@
                 </div>
             </div>
             <div class="t-plp__grid js-plp-grid show-product" id="show-product">
+                
                 @foreach ($product_list as $value)
-                <div class="t-plp__product margin_top_list" data-animate-grid-id="0.7226111054869209" style="transform-origin: 0px 0px;">
+                <div class="loadmore1 t-plp__product margin_top_list" data-animate-grid-id="0.7226111054869209" style="transform-origin: 0px 0px;">
                     <div class="views-field views-field-search-api-rendered-item" style="transform-origin: 0px 0px;">
                         <span class="field-content">
-                            <div data-product-name="{{ $value->name }}" data-product-sku="{{ $value->id }}" data-product-brand="frontiercoop_market" data-product-category="\Accessories\Home and Pet\Kitchen and Dining\Food Storage and Containers" class="m-product-card">
+                            <div data-product-name="{{ $value->name }}" data-product-sku="{{ $value->id }}" data-product-brand="frontiercoop_market" data-product-category="{{ get_category_id($value->id) }}" class="m-product-card">
                                 <div class="m-product-card__content-wrapper">
                                     <a class="m-product-card__img-wrapper" href="san-pham-10" title="{{ $value->name }}">
                                         <img class="m-product-card__img ls-is-cached lazyloaded" data-src="https://wall.vn/wp-content/uploads/2020/03/hinh-nen-dep-may-tinh-1.jpg" alt="{{ $value->name }}" src="https://wall.vn/wp-content/uploads/2020/03/hinh-nen-dep-may-tinh-1.jpg">
@@ -98,37 +99,51 @@
                 </div>
                 @endforeach
             </div>
-    <div class="t-plp__grid js-plp-grid">
-    </div>
-    <div class="t-plp__media-block" data-animate-grid-id="0.7649058207806405" style="transform-origin: 0px 0px;">
-        <div class="t-plp__pagination">
-            <div class="m-pagination">
-                <ul class="m-pagination__list">
-                    @if ($current_page > 1 && $total_page > 1)
-                    <li class="m-pagination__list-item">
-                        <a class="m-pagination__link" href="{{ route('get_user.productlist', ['page'=>$current_page-1]) }}" title="Current page" aria-current="page">Prev</a>
-                    </li>
-                    @endif 
-                    @for ($i = 1; $i <= $total_page; $i++)
-                        @if ($i==$current_page) 
-                        <li class="m-pagination__list-item m-pagination__list-item--active">
-                        <a class="m-pagination__link" href="#" title="Go to page {{ $i }}">{{ $i }}</a>
-                        </li>
-                        @else 
-                        <li class="m-pagination__list-item">
-                            <a class="m-pagination__link" href="{{ route('get_user.productlist', ['page'=>$i]) }}" title="Go to page {{ $i }}">{{ $i }}</a>
-                        </li>
-                        @endif
-                    @endfor
-                    @if ($current_page < $total_page && $total_page > 1)
-                        <li class="m-pagination__list-item">
-                            <a class="m-pagination__link" href="{{ route('get_user.productlist', ['page'=>$current_page+1]) }}" title="Go to page next">Next</a>
-                        </li>
-                    @endif
-                </ul>
+            <div >
+                <a href="#" id="loadMore">Xem Thêm</a>
             </div>
-        </div>
-    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+    <script>
+        function get_data1(){
+            var sort_by = $('#sort_by').val();
+            var order_by = $('#order_by').val();
 
+            $.get( "{{ route('get_user.productlist_filter') }}", { sort_by: sort_by, order_by: order_by } )
+                .done(function( data ) {
+                $( "#show-product" ).html( data );
+            });
+        }
+        
+       
+        $(function () {
+            $(".loadmore1").slice(0, 4).show();
+                $("#loadMore").on("click", function (e) {
+                    e.preventDefault();
+                    $(".loadmore1:hidden").slice(0, 4).slideDown();
+                    if ($(".loadmore1:hidden").length == 0) {
+                        $("#load").fadeOut("slow");
+                    }
+                    $("#show-product").animate({
+                        scrollTop: $(this).offset().top
+                    }, 1500);
+                });
+            });
+
+            $("a[href=#top]").click(function () {
+                $("#show-product").animate({
+                    scrollTop: 0
+                }, 600);
+                return false;
+            });
+
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 50) {
+                    $(".totop a").fadeIn();
+                } else {
+                    $(".totop a").fadeOut();
+                }
+            });
+    </script>
 </main>
+
 @stop
