3123515bd2e7069ffce4bebe668b56bf0b9a0b24
ducanh30091995hpv <ducanh30091995hpv@gmail.com>
2021-07-29 16:06:01 +0700

    user_comment_ducanh_3

--- resources/views/pages/product/index.blade.php
+++ resources/views/pages/product/index.blade.php
@@ -196,55 +196,8 @@
                                                         </div>
                                                         <p class="margin-left1">consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
                                                             lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                                        <div class="pad-ver">
-                                                            <div class="btn-group">
-                                                                <a class="btn btn-sm btn-default btn-hover-success" href="#"><i class="fa fa-thumbs-up"></i></a>
-                                                                <a class="btn btn-sm btn-default btn-hover-danger" href="#"><i class="fa fa-thumbs-down"></i></a>
-                                                            </div>
-                                                            <a class="btn btn-sm btn-default btn-hover-primary" href="#">Comment</a>
-                                                        </div>
+
                                                         <hr>
-
-                                                        <!-- Comments -->
-                                                        <div>
-                                                            <div class="media-block">
-                                                                <a class="media-left" href="#"><img class="img-circle img-sm" alt="Profile Picture" src="https://bootdey.com/img/Content/avatar/avatar2.png"></a>
-                                                                <div class="media-body">
-                                                                    <div class="mar-btm">
-                                                                        <a href="#" class="btn-link text-semibold media-heading box-inline">Bobby Marz</a>
-                                                                    </div>
-                                                                    <p class="margin-left1">Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
-                                                                        ex ea commodo consequat.</p>
-                                                                    <div class="pad-ver">
-                                                                        <div class="btn-group">
-                                                                            <a class="btn btn-sm btn-default btn-hover-success active" href="#"><i class="fa fa-thumbs-up"></i> You Like it</a>
-                                                                            <a class="btn btn-sm btn-default btn-hover-danger" href="#"><i class="fa fa-thumbs-down"></i></a>
-                                                                        </div>
-                                                                        <a class="btn btn-sm btn-default btn-hover-primary" href="#">Comment</a>
-                                                                    </div>
-                                                                    <hr>
-                                                                </div>
-                                                            </div>
-
-                                                            <div class="media-block">
-                                                                <a class="media-left" href="#"><img class="img-circle img-sm" alt="Profile Picture" src="https://bootdey.com/img/Content/avatar/avatar3.png">
-                                                                </a>
-                                                                <div class="media-body">
-                                                                    <div class="mar-btm">
-                                                                        <a href="#" class="btn-link text-semibold media-heading box-inline">Lucy Moon</a>
-                                                                    </div>
-                                                                    <p class="margin-left1">Duis autem vel eum iriure dolor in hendrerit in vulputate ?</p>
-                                                                    <div class="pad-ver">
-                                                                        <div class="btn-group">
-                                                                            <a class="btn btn-sm btn-default btn-hover-success" href="#"><i class="fa fa-thumbs-up"></i></a>
-                                                                            <a class="btn btn-sm btn-default btn-hover-danger" href="#"><i class="fa fa-thumbs-down"></i></a>
-                                                                        </div>
-                                                                        <a class="btn btn-sm btn-default btn-hover-primary" href="#">Comment</a>
-                                                                    </div>
-                                                                    <hr>
-                                                                </div>
-                                                            </div>
-                                                        </div>
                                                     </div>
                                                 </div>
                                                 <!--===================================================-->
