510f892902de1ead935b72b63e1d1492470b86e3
dainx217 <nguyenxuandai217@gmail.com>
2021-08-24 17:42:27 +0700

    gưi mail

--- config/mail.php
+++ config/mail.php
@@ -87,7 +87,7 @@ return [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
-
+   
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
