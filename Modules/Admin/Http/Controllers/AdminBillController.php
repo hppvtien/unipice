88f4e47a36740518652df46564216218c95c82e5
unknown <hppvtien@gmail.com>
2021-08-28 17:29:02 +0700

    add-inventory

--- Modules/Admin/Http/Controllers/AdminBillController.php
+++ Modules/Admin/Http/Controllers/AdminBillController.php
@@ -15,17 +15,74 @@ class AdminBillController extends AdminController
 {
     public function index()
     {
-        $bill = Uni_LotProduct::orderBy('created_at','asc')->get();
-        return view('admin::pages.bill.index', compact('bill'));
+        $total_top = 0;
+        $total_mid = 0;
+        $total_bot = 0;
+        $bill = Uni_LotProduct::orderBy('created_at', 'asc')->get();
+        foreach ($bill as $item) {
+            $total_top += $item->total_qty * $item->price_lotproduct;
+            $total_mid += ($item->total_qty - $item->qty) * $item->price_lotproduct;
+            $total_bot += $item->qty * $item->price_lotproduct;
+        }
+        $viewData = [
+            'bill' => $bill,
+            'total_top' => $total_top,
+            'total_mid' => $total_mid,
+            'total_bot' => $total_bot
+        ];
+        return view('admin::pages.bill.index', $viewData);
+    }
+    public function ajax_index(Request $request)
+    {
+        $from_date = $request->from_date;
+        $to_date = $request->to_date;
+        $range_date = $request->range_date;
+        $total_top = 0;
+        $total_mid = 0;
+        $total_bot = 0;
+        if ($range_date) {
+            $bill = Uni_LotProduct::whereBetween('updated_at', [Carbon::now()->subDays($range_date), Carbon::now()])->get();
+            foreach ($bill as $item) {
+                $total_top += $item->total_qty * $item->price_lotproduct;
+                $total_mid += ($item->total_qty - $item->qty) * $item->price_lotproduct;
+                $total_bot += $item->qty * $item->price_lotproduct;
+            }
+        } elseif ($to_date != '' && $from_date != '') {
+            $bill = Uni_LotProduct::whereBetween('updated_at', [$from_date, $to_date])->get();
+            foreach ($bill as $item) {
+                $total_top += $item->total_qty * $item->price_lotproduct;
+                $total_mid += ($item->total_qty - $item->qty) * $item->price_lotproduct;
+                $total_bot += $item->qty * $item->price_lotproduct;
+            }
+        } else {
+            $bill = Uni_LotProduct::where('status', 2)->get();
+            foreach ($bill as $item) {
+                $total_top += $item->total_qty * $item->price_lotproduct;
+                $total_mid += ($item->total_qty - $item->qty) * $item->price_lotproduct;
+                $total_bot += $item->qty * $item->price_lotproduct;
+            }
+        }
+        $viewData = [
+            'bill' => $bill,
+            'total_top' => $total_top,
+            'total_mid' => $total_mid,
+            'total_bot' => $total_bot
+        ];
+        if ($viewData != 0) {
+            $html = view('admin::pages.bill.ajax_index', $viewData)->render();
+        } else {
+            $html = 'Doanh thu chưa có vui lòng bán hàng';
+        }
+        return $html;
     }
     public function index_order()
     {
-        $order = Uni_Order::where('status',2)->pluck('total_money');
-        
+        $order = Uni_Order::where('status', 2)->pluck('total_money');
+
         $order_total = 0;
-     foreach($order as $item){
-        $order_total += (int)str_replace('.','',$item); 
-     }
+        foreach ($order as $item) {
+            $order_total += (int)str_replace('.', '', $item);
+        }
         return view('admin::pages.bill.index_order', compact('order_total'));
     }
 
@@ -36,54 +93,35 @@ class AdminBillController extends AdminController
         $range_date = $request->range_date;
         $type_pay = $request->type_pay;
         $order_total = 0;
-        if($range_date) {
-            $type_range = 'trong '.$range_date.' ngày trước tới hiện tại';
-            $uni_order = Uni_Order::whereBetween('updated_at', [Carbon::now()->subDays($range_date),Carbon::now()])->where('status',2)->pluck('total_money');
-            foreach($uni_order as $item){
-                $order_total += (int)str_replace('.','',$item); 
-             }
-        } elseif($to_date != '' && $from_date != ''){
-            $type_range = 'trong khoảng thời gian '.$to_date.'-'.$from_date;
-            $uni_order = Uni_Order::whereBetween('updated_at', [$from_date,$to_date])->where('status',2)->pluck('total_money');
-            foreach($uni_order as $item){
-                $order_total += (int)str_replace('.','',$item); 
-             }
-        } elseif($type_pay) {
-            $type_range = 'của hình thức thanh toán -'.$type_pay;
-            $uni_order = Uni_Order::where('type_pay', $type_pay)->where('status',2)->pluck('total_money');
-            foreach($uni_order as $item){
-                $order_total += (int)str_replace('.','',$item); 
-             }
+        if ($range_date) {
+            $type_range = 'trong ' . $range_date . ' ngày trước tới hiện tại';
+            $uni_order = Uni_Order::whereBetween('updated_at', [Carbon::now()->subDays($range_date), Carbon::now()])->where('status', 2)->pluck('total_money');
+            foreach ($uni_order as $item) {
+                $order_total += (int)str_replace('.', '', $item);
+            }
+        } elseif ($to_date != '' && $from_date != '') {
+            $type_range = 'trong khoảng thời gian từ ' . $to_date . ' tới ' . $from_date;
+            $uni_order = Uni_Order::whereBetween('updated_at', [$from_date, $to_date])->where('status', 2)->pluck('total_money');
+            foreach ($uni_order as $item) {
+                $order_total += (int)str_replace('.', '', $item);
+            }
+        } elseif ($type_pay) {
+            $type_range = 'của hình thức thanh toán ' . $type_pay;
+            $uni_order = Uni_Order::where('type_pay', $type_pay)->where('status', 2)->pluck('total_money');
+            foreach ($uni_order as $item) {
+                $order_total += (int)str_replace('.', '', $item);
+            }
         } else {
-            $uni_order = Uni_Order::where('status',2)->pluck('total_money');
-            foreach($uni_order as $item){
-                $order_total += (int)str_replace('.','',$item); 
-             }
+            $uni_order = Uni_Order::where('status', 2)->pluck('total_money');
+            foreach ($uni_order as $item) {
+                $order_total += (int)str_replace('.', '', $item);
+            }
         }
-        if($order_total != 0){
-            $html = 'Tổng doanh thu '.$type_range.': <span class="text-danger">'.formatVnd($order_total).'</span>';
+        if ($order_total != 0) {
+            $html = 'Tổng doanh thu ' . $type_range . ': <span class="text-danger">' . formatVnd($order_total) . '</span>';
         } else {
             $html = 'Doanh thu chưa có vui lòng bán hàng';
         }
         return $html;
     }
-    public function delete(Request $request, $id)
-    {
-        if($request->ajax())
-        {
-            $category = Bill::find($id);
-            if ($category)
-            {
-                $category->delete();
-                RenderUrlSeoCourseService::deleteUrlSeo(SeoEdutcation::TYPE_CATEGORY, $id);
-            }
-            return response()->json([
-                'status' => 200,
-                'message' => 'Xoá dữ liệu thành công'
-            ]);
-        }
-
-        return redirect()->to('/');
-    }
-
 }
--- Modules/Admin/Resources/views/pages/bill/index.blade.php
+++ Modules/Admin/Resources/views/pages/bill/index.blade.php
@@ -5,8 +5,30 @@
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
             <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">Danh sách hóa đơn</h4>
+                <h4 class="content-title mb-0 my-auto">Tồn kho</h4>
                 <span class="text-muted mt-1 tx-13 ml-2 mb-0">/ index</span>
+            </div>
+        </div>
+        <div class="d-flex my-xl-auto right-content col-2">
+            <div>
+                <select name="range_date" id="range_date" class="form-control">
+                    <option title="7 ngày gần nhất" value="" selected>Khoảng thời gian</option>
+                    <option title="7 ngày gần nhất" value="7">7 ngày gần nhất</option>
+                    <option title="30 ngày gần nhất" value="30">30 ngày gần nhất</option>
+                    <option title="6 tháng gần nhất" value="180">6 Tháng gần nhất</option>
+                    <option title="1 năm gần nhât" value="365">1 năm gần nhât</option>
+                </select>
+            </div>
+        </div>
+        <input class="form-control" id="search_k" type="hidden" data-url="{{ route('get_admin.bill.ajax_index') }}" placeholder="Tìm kiếm theo tên khách hàng" aria-label="Search">
+        <div class="md-form form-sm form-2 pl-0 col-3" style="margin: auto;">
+            <div class="form-group">
+                <label for="exampleInputEmail1" class="required">Từ ngày <span>(*)</span></label>
+                <input type="date" class="form-control keypress-count" value="" name="from_date">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1" class="required">Tới ngày <span>(*)</span></label>
+                <input type="date" class="form-control keypress-count" value="" name="to_date">
             </div>
         </div>
     </div>
@@ -16,8 +38,7 @@
         <div class="col-xl-12">
             <div class="card">
                 <div class="card-body">
-                    <div class="table-responsive">
-
+                    <div class="table-responsive" id="show-search">
                         <table class="table mg-b-0 text-md-nowrap">
                             <thead>
                                 <tr>
@@ -25,7 +46,7 @@
                                     <th rowspan="1"></th>
                                     <th colspan="3" class="text-center border-bottom border-left">Đầu kỳ</th>
                                     <th colspan="3" class="text-center border-bottom border-left">Trong kỳ</th>
-                                    <th colspan="3" class="text-center border-bottom border-left  border-right">Cuối kỳ kỳ</th>
+                                    <th colspan="3" class="text-center border-bottom border-left  border-right">Cuối kỳ</th>
 
                                 </tr>
 
@@ -35,11 +56,11 @@
                                     <td class="border"></td>
                                     <td class="border"></td>
                                     <td class="font-weight-bold">Tổng tiền đầu kỳ:</td>
-                                    <td colspan="2" class="border-right text-danger">10000000000</td>
+                                    <td colspan="2" class="border-right text-danger">{{ formatVnd($total_top) }}</td>
                                     <td class="font-weight-bold">Tổng tiền trong kỳ:</td>
-                                    <td colspan="2" class="border-right text-danger">10000000000</td> 
+                                    <td colspan="2" class="border-right text-danger">{{ formatVnd($total_mid) }}</td> 
                                     <td class="font-weight-bold">Tổng tiền cuối kỳ:</td>
-                                    <td colspan="2" class="border-right text-danger">10000000000</td>
+                                    <td colspan="2" class="border-right text-danger">{{ formatVnd($total_bot) }}</td>
                                 </tr>
                                 <tr>
                                     <td class="border">ID</td>
@@ -59,14 +80,14 @@
                                     <td scope="row">{{ $item->id }}</td>
                                     <td scope="row">{{ getNameProduct($item->product_id) }}</td>
                                     <td scope="row">{{ $item->total_qty }}</td>
-                                    <td scope="row">{{ $item->price_lotproduct }}</td>
-                                    <td scope="row">{{ $item->total_qty*$item->price_lotproduct }}</td>
+                                    <td scope="row">{{ formatVnd($item->price_lotproduct) }}</td>
+                                    <td scope="row">{{ formatVnd($item->total_qty*$item->price_lotproduct) }}</td>
                                     <td scope="row">{{ $item->total_qty-$item->qty }}</td>
-                                    <td scope="row">{{ $item->price_lotproduct }}</td>
-                                    <td scope="row">{{ ($item->total_qty-$item->qty)*$item->price_lotproduct }}</td>
+                                    <td scope="row">{{ formatVnd($item->price_lotproduct) }}</td>
+                                    <td scope="row">{{ formatVnd(($item->total_qty - $item->qty)*$item->price_lotproduct) }}</td>
                                     <td scope="row">{{ $item->qty }}</td>
-                                    <td scope="row">{{ $item->price_lotproduct }}</td>
-                                    <td scope="row">{{ $item->total_qty*$item->price_lotproduct }}</td>
+                                    <td scope="row">{{ formatVnd($item->price_lotproduct) }}</td>
+                                    <td scope="row">{{ formatVnd($item->qty*$item->price_lotproduct) }}</td>
                                 </tr>
                                 @endforeach
                             </tbody>
